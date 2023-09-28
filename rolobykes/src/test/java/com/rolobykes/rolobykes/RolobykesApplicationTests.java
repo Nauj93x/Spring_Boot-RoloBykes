04206cd66d1a84f55732270725c802684d59b037
Gustavo Ortiz <gustavo.ortiz7319@gmail.com>
2023-09-28 22:41:18 +0000

    Se agreg proyecto de spring boot

---
+++ rolobykes/src/test/java/com/rolobykes/rolobykes/RolobykesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolobykes.rolobykes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolobykesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
