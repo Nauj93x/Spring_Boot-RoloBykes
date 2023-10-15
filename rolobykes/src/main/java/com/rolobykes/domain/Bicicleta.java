b14beb56334f88d99154b289f99d8e98314f087e
rasamuel <ra.samuel@javeriana.edu.co>
2023-10-15 16:14:43 +0000

    main nuevo

--- rolobykes/src/main/java/com/rolobykes/domain/Bicicleta.java
+++ rolobykes/src/main/java/com/rolobykes/domain/Bicicleta.java
@@ -1,5 +1,6 @@
 package com.rolobykes.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
--- rolobykes/src/main/java/com/rolobykes/domain/Reserva.java
+++ rolobykes/src/main/java/com/rolobykes/domain/Reserva.java
@@ -1,5 +1,6 @@
 package com.rolobykes.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
